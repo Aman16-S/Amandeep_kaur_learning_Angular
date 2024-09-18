bc17d6b995f4b75794f2222d9ffb775d5e79caca
Aman16-S <W0836054@myscc.ca>
2024-09-18 18:05:44 -0400

    Assignment 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Assignment1_learning_Angular';
+  name: string = 'Amandeep kaur';
+  age: number = 23;
+}
