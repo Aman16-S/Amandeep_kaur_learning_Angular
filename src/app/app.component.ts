3dfeb1e84056b410563c3c28cde449a6398e1006
Aman16-S <W0836054@myscc.ca>
2024-09-18 19:15:05 -0400

    Final Assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {book} from "../book";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Assignment1_learning_Angular';
-  name: string = 'Amandeep kaur';
-  age: number = 23;
+  title = 'Book Generation';
+
+  books: book[]  = [
+   {id: 1, bookName: 'The Great Getsby', type: 'fiction', authorName: 'F. Scott Fitzgerald', sale: 100},
+   {id: 2, bookName: '1984', type: 'Dystopian', authorName: 'George Orwell', sale: 200},
+   {id: 3, bookName: 'To Kill a Mockingbird', type: 'fiction', authorName: 'Harper Lee', sale: 150},
+   {id: 4, bookName: 'The Catcher in the Rye', type: 'fiction', authorName: 'J.D. Salinger', sale: 75},
+   {id: 5, bookName: 'Pride and Prejudice', type: 'Romance', authorName: 'Jane Austen', sale: 120},
+   {id: 6, bookName: 'The Hobbit', type: 'Fantasy', authorName: 'J.R.R. Tolkien', sale: 300},
+];
 }
+
+
