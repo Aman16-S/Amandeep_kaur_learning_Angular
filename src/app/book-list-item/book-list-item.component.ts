105234e4129417cf1d0a69e07742d3180e974ce2
Aman16-S <W0836054@myscc.ca>
2024-09-25 17:48:45 -0400

    Generate two comment

--- src/app/book-list-item/book-list-item.component.ts
+++ src/app/book-list-item/book-list-item.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+
+let input = Input();
+
+
+
 
 @Component({
   selector: 'app-book-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './book-list-item.component.html',
   styleUrl: './book-list-item.component.css'
 })
 export class BookListItemComponent {
-
+  /**
+   * Input marks the property as an input property
+   * that can receive data from a parent
+   * ? says it is optional
+   */
+  @Input() item: any;
 }
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { NgForOf } from "@angular/common";
+import { BookListItemComponent } from "../book-list-item/book-list-item.component";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    BookListItemComponent
+  ],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.css'
+  styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent {
+
+  bookItems = [
+    { id: 1, bookName: 'Norwegian Wood', type: 'Fiction', authorName: 'Haruki Murakami', sale: 12000 },
+    { id: 2, bookName: 'The Great Gatsby', type: 'Classic', authorName: 'F. Scott Fitzgerald', sale: 18000 },
+    { id: 3, bookName: '1984', type: 'Dystopian', authorName: 'George Orwell', sale: 22000 },
+    { id: 4, bookName: 'The Alchemist', type: 'Adventure', authorName: 'Paulo Coelho', sale: 15000 },
+  ];
+
+
+  selectedBookItem: any;
+
+  selectBookItem(item: any) {
+    this.selectedBookItem = item;
+  }
 
 }
