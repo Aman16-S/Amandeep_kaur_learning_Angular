0df59dccc0035ac01256913a7914bf21202c19c3
W0836054 <W08360542@>
2024-10-09 15:52:35 -0400

    Just completed till step 5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import {provideRouter, Routes} from '@angular/router';
 import { AppComponent } from './app/app.component';
+import {BookListItemComponent} from "./app/book-list-item/book-list-item.component";
+import {BookListComponent} from "./app/book-list/book-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path:'', redirectTo:'/books', pathMatch: 'full'}
+  {path:'books', component: BookListComponent},
+  {path:'books/:id', component: BookListItemComponent}
+]
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter (routes)]
+}).then(r=> console.log('Bootstrap successful'));
