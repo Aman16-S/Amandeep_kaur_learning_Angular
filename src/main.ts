4541884f3e0d088e282eb06b56e6e93f53c7e3a7
W0836054 <W08360542@>
2024-10-09 16:13:57 -0400

    Just completed till step 9 and not know how to do step 10 and this time, I have completed the assignment by myself

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import {provideRouter, Routes} from '@angular/router';
 import { AppComponent } from './app/app.component';
 import {BookListItemComponent} from "./app/book-list-item/book-list-item.component";
 import {BookListComponent} from "./app/book-list/book-list.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
 
 const routes: Routes = [
   {path:'', redirectTo:'/books', pathMatch: 'full'}
   {path:'books', component: BookListComponent},
-  {path:'books/:id', component: BookListItemComponent}
+  {path:'books/:id', component: BookListItemComponent},
+  {path: 'modify-list-item', componet: ModifyListItemComponent},
+  {path: '**', component:PageNotFoundComponent}// Wildcard route for a 404 page
+
 ]
 bootstrapApplication(AppComponent, {
   providers: [provideRouter (routes)]
